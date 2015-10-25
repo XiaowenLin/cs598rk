191a55d9dfde0150179e5cce104bc196723d6bf6
xlin <xlin11@illinois.edu>
2015-10-24 22:38:39 -0500

    update expore based on conversion to json methods provided on data website

--- explore_products.py
+++ explore_products.py
@@ -1,23 +1,36 @@
 import json
-import re
 
 # filename
 base_dir = os.path.join('data') 
-input_path = os.path.join('meta_Musical_Instruments.json') 
+input_path = os.path.join('meta_musical.json') 
 file_name = os.path.join(base_dir, input_path)
 
 # load data
 num_part = 2
 r_prods = sc.textFile(file_name, num_part)
-def prepare_str(s):
-	s_ = re.sub('\'([a-z]*) ', r"##39##\1 ", s)
-	return re.sub('"""*', r"#34#34#", s_).replace('\'', '"')
-
-
-r_prods_ = r_prods.map(prepare_str)
-prods = r_prods_.map(json.loads)
+prods = r_prods.map(json.loads)
 prods.take(7)
 prods.cache()
 """
 u"{'asin': '0006428320', 'title': 'Six Sonatas For Two Flutes Or Violins, Volume 2 (#4-6)', 'price': 17.95, 'imUrl': 'http://ecx.images-amazon.com/images/I/41EpRmh8MEL._SY300_.jpg', 'salesRank': {'Musical Instruments': 207315}, 'categories': [['Musical Instruments', 'Instrument Accessories', 'General Accessories', 'Sheet Music Folders']]}"
 """
+
+# number of products
+asin = prods.map( lambda x: (x.get('asin'), x.get('price'), x.get('categories')) )
+assert(asin.filter(lambda (asin, price, catg): asin == None).count() == 0)
+asin_cnt = asin.map(lambda (asi, pri, cat): (asi, 1)).reduceByKey(lambda x, y: x + y)
+num_prod = asin_cnt.count() # 84901
+
+# number of categories
+categ = asin.flatMap( lambda (asin, price, catg): catg ).flatMap( lambda x: x ).map( lambda x: (x, 1) )
+categ_n = categ.reduceByKey(lambda x, y: x + y)
+catg_num = categ_n.count() # 1114
+
+# (catg, 1)
+categ_n
+
+# (catg, ave price)
+categ_p = asin.map( lambda (asin, price, catg): ([item for sublist in catg for item in sublist], price) ).flatMap( lambda (catg, price): [(ct, (price, 1)) for ct in catg] ).filter(lambda (ct, (price, i)): price != None)
+categ_totalp = categ_p.reduceByKey( lambda x, y: (x[0] + y[0], x[1] + y[1]) )
+categ_avep = categ_totalp.map(lambda (ct, (p, c)): (ct, p/c))
+
--- explore_reviews.py
+++ explore_reviews.py
@@ -1,33 +1,37 @@
 import json
+import re
 
 # filename
 base_dir = os.path.join('data') 
-input_path = os.path.join('reviews_Musical_Instruments.json.gz') 
+input_path = os.path.join('meta_musical.json') 
 file_name = os.path.join(base_dir, input_path)
 
 # load data
 num_part = 2
-reviews = sc.textFile(file_name, num_part)
-reviews = reviews.map(json.loads)
-reviews.cache()
-
+r_prods = sc.textFile(file_name, num_part)
+prods = r_prods.map(json.loads)
+prods.take(7)
+prods.cache()
 """
-[{u'reviewerID': u'A1YS9MDZP93857', u'asin': u'0006428320', u'reviewerName': u'John Taylor', u'helpful': [0, 0], u'reviewText': u'The portfolio is fine except for the fact that the last movement of sonata #6 is missing. What should one expect?', u'overall': 3.0, u'summary': u'Parts missing', u'unixReviewTime': 1394496000, u'reviewTime': u'03 11, 2014'}]"""
+u"{'asin': '0006428320', 'title': 'Six Sonatas For Two Flutes Or Violins, Volume 2 (#4-6)', 'price': 17.95, 'imUrl': 'http://ecx.images-amazon.com/images/I/41EpRmh8MEL._SY300_.jpg', 'salesRank': {'Musical Instruments': 207315}, 'categories': [['Musical Instruments', 'Instrument Accessories', 'General Accessories', 'Sheet Music Folders']]}"
+"""
 
-# find a product
-reviews.filter(lambda x: x[u'asin'] == u'0006428320').collect()
+# number of products
+asin = prods.map( lambda x: (x.get('asin'), x.get('price'), x.get('categories')) )
+assert(asin.filter(lambda (asin, price, catg): asin == None).count() == 0)
+asin_cnt = asin.map(lambda (asi, pri, cat): (asi, 1)).reduceByKey(lambda x, y: x + y)
+num_prod = asin_cnt.count() # 84901
 
-# numb of reviews of each product
-rating = reviews.map(lambda x: (x[u'asin'], [x[u'overall']]))
-ratings = rating.reduceByKey(lambda x, y: x + y) # [(u'B000I5562E', [4.0, 1.0, 1.0, 2.0]), (u'B00005K3P6', [5.0]), (u'B000JFEAIO', [5.0, 5.0])]
-ave_rating = ratings.map( lambda (x, y): (x, sum(y), len(y)) ) # [(u'B000I5562E', 8.0, 4), (u'B00005K3P6', 5.0, 1), (u'B000JFEAIO', 10.0, 2)]
+# number of categories
+categ = asin.flatMap( lambda (asin, price, catg): catg ).flatMap( lambda x: x ).map( lambda x: (x, 1) )
+categ_n = categ.reduceByKey(lambda x, y: x + y)
+catg_num = categ_n.count() # 1114
 
-# top 10 product
-ave_rating.sortBy(lambda (asin, tot, cnt): tot, ascending=False).map(lambda (asin, tot, cnt): (asin, tot / cnt)).take(10) # [(u'B000ULAP4U', 4.703945500993472), (u'B003VWJ2K8', 4.592527472527473), (u'B003VWKPHC', 4.5745477230193385), (u'B001MSS6CS', 4.295774647887324), (u'B00FPPQYXM', 4.706293706293707), (u'B009E3EWPI', 4.6375838926174495), (u'1417030321', 4.448275862068965), (u'B002VA464S', 4.472795497185741), (u'B00063678K', 4.638922888616891), (u'B005CX4GLE', 4.639506172839506)]
+# (catg, 1)
+categ_n
 
-# average rating
-ave_rating.map(lambda (asin, tot, cnt): tot).reduce(lambda x, y: x + y) / ave_rating.count()
-
-# number of reviews of each product
-ave_rating.map(lambda (asin, tot, cnt): cnt).reduce(lambda x, y: x + y) / ave_rating.count()
+# (catg, ave price)
+categ_p = asin.map( lambda (asin, price, catg): ([item for sublist in catg for item in sublist], price) ).flatMap( lambda (catg, price): [(ct, (price, 1)) for ct in catg] ).filter(lambda (ct, (price, i)): price != None)
+categ_totalp = categ_p.reduceByKey( lambda x, y: (x[0] + y[0], x[1] + y[1]) )
+categ_avep = categ_totalp.map(lambda (ct, (p, c)): (ct, p/c))
 
