ec0cf5fe614d04c0e280544e49971752054be658
hidykong <hidykong@gmail.com>
2015-11-12 21:29:14 -0600

    incorporating comments

--- projectFolder/product/views.py
+++ projectFolder/product/views.py
@@ -13,13 +13,14 @@ def product_list(request):
 
 def viz(request, product):
 	#reader = csv.reader("data/cellphone.csv")
-	'''
+	
 	with open('product/data/cellphone.json') as data_file:    
 	    data = json.load(data_file)
 	    data = json.dumps(data)
 
 	data = data.replace("&quot;", "\"")
-	'''
+	posList = data["pos"]
+	
 	return render(request, 'product/visualization.html', {
             'product': product,
             #'data': data,
